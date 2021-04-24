00eb4ed89e60f7b761a1f20dcd464d9481d93eed
Antoine Poinsot <darosior@protonmail.com>
2021-04-24 14:35:22 +0200

    Add UDS support also for Windows targets
    This is quite a hack, and requires a hacky third party crate until the
    Rust standard library includes Windows support for UDS.
    
    This is activated only when the 'simple_uds' feature is triggered on a
    Windows target, and automatically activated in this case.
    
    This breaks the MSRV for this specific configuration, jumping it from
    1.29 to 1.30
    
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,8 @@ pub mod simple_tcp;
 
 #[cfg(feature = "simple_uds")]
 pub mod simple_uds;
+#[cfg(all(target_os = "windows", feature = "simple_uds"))]
+extern crate uds_windows;
 
 // Re-export error type
 pub use error::Error;
